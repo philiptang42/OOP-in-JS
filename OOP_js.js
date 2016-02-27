088c8148068cef65f94aaa8eebe42bcebd10c261
philiptang42 <philiptang42@gmail.com>
2016-02-26 23:41:55 -0500

    added speak function to the object

--- OOP_js.js
+++ OOP_js.js
@@ -7,6 +7,9 @@ var james = {
 function Person(job, married) {
     this.job = job;
     this.married = married;
+    this.speak = function() {
+      console.log("Hello!")
+    }
 }
 
 // create a "gabby" object using the Person constructor!
