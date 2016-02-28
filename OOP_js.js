cd25faaecf61083aa690f57a5387fac142946c10
philiptang42 <philiptang42@gmail.com>
2016-02-27 22:29:12 -0500

    referencing and changing property values

--- OOP_js.js
+++ OOP_js.js
@@ -29,3 +29,20 @@ var james = {
 
 james.speak("great");
 james.speak("just okay");
+----------------------------
+var james = {
+    job: "programmer",
+    married: false,
+    sayJob: function() {
+      console.log("Hi, I work as a " + this.job);
+    }
+};
+
+// james' first job
+james.sayJob();
+
+// change james' job to "super programmer" here
+james.job = "super programmer"
+
+// james' second job
+james.sayJob();
