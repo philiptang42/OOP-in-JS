3bb83d7c89ee744cd24990c109b485a7ff401b2d
philiptang42 <philiptang42@gmail.com>
2016-02-29 22:14:20 -0500

    using a for in loop to list all the values of of each property in an object

--- OOP_js.js
+++ OOP_js.js
@@ -97,3 +97,15 @@ var nyc = {
 for(var property in nyc) {
   console.log(property);
 };
+-------------------------------
+var nyc = {
+    fullName: "New York City",
+    mayor: "Bill de Blasio",
+    population: 8000000,
+    boroughs: 5
+};
+
+// write a for-in loop to print the value of nyc's properties
+for(var property in nyc) {
+  console.log(nyc[property]);
+};
