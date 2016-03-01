682af80c1919aaeb3bc21a12d951c1f057a3f3b2
philiptang42 <philiptang42@gmail.com>
2016-02-29 22:18:59 -0500

    object constructors essentially create new classes

--- OOP_js.js
+++ OOP_js.js
@@ -109,3 +109,17 @@ var nyc = {
 for(var property in nyc) {
   console.log(nyc[property]);
 };
+-------------------------------
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+}
+
+// Let's make bob again, using our constructor
+var bob = new Person("Bob Smith", 30);
+var susan = new Person("Susan Jordan", 35);
+
+// make your own class here
+function Circle(radius) {
+  this.radius = radius
+};
