1e46765af0f20e40f828809f52d2779fbab8541d
philiptang42 <philiptang42@gmail.com>
2016-03-01 23:11:06 -0500

    using classes

--- OOP_js.js
+++ OOP_js.js
@@ -143,3 +143,20 @@ var snoopy = new Dog("Beagle");
   };
 // this causes an error, because snoopy doesn't know how to bark!
 snoopy.bark();
+-----------------------------
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+}
+// a function that prints the name of any given person
+var printPersonName = function (p) {
+  console.log(p.name);
+};
+
+var bob = new Person("Bob Smith", 30);
+printPersonName(bob);
+
+// make a person called me with your name and age
+// then use printPersonName to print your name
+var me = new Person("Philip", 22);
+printPersonName(me);
