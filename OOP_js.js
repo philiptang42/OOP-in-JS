3c6b185761fe75206b0bb315caa3921e7f60b1d9
philiptang42 <philiptang42@gmail.com>
2016-03-01 23:51:42 -0500

    using inheritance

--- OOP_js.js
+++ OOP_js.js
@@ -178,4 +178,17 @@ Cat.prototype.meow = function() {
 
 // add code here to make the cats meow!
 cheshire.meow
-gary.meow
+---------------------------
+// create your Animal class here
+function Animal(name, numLegs) {
+  this.name = name;
+  this.numLegs = numLegs;
+};
+// create the sayName method for Animal
+Animal.prototype.sayName = function() {
+  console.log("Hi my name is " + this.name);
+};
+
+// provided code to test above constructor and method
+var penguin = new Animal("Captain Cook", 2);
+penguin.sayName();
