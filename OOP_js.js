abb8bb943582426ca7b44d0ec4a22b66cf14dad1
philiptang42 <philiptang42@gmail.com>
2016-03-04 21:04:38 -0500

    returning private methods

--- OOP_js.js
+++ OOP_js.js
@@ -320,3 +320,25 @@ console.log(john.bankBalance);
 // create a new variable myBalance that calls getBalance()
 var myBalance = john.getBalance();
 console.log(myBalance);
+----------------------------
+function Person(first,last,age) {
+   this.firstname = first;
+   this.lastname = last;
+   this.age = age;
+   var bankBalance = 7500;
+
+   var returnBalance = function() {
+      return bankBalance;
+   };
+
+   // create the new function here
+   this.askTeller = function() {
+      return returnBalance
+   };
+}
+
+var john = new Person('John','Smith',30);
+console.log(john.returnBalance);
+var myBalanceMethod = john.askTeller();
+var myBalance = myBalanceMethod();
+console.log(myBalance);
