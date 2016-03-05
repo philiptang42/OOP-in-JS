c23d15d2ebe0a639c5c24ce73a3016c4027019c4
philiptang42 <philiptang42@gmail.com>
2016-03-04 21:23:53 -0500

    accessing private method with a password

--- OOP_js.js
+++ OOP_js.js
@@ -342,3 +342,20 @@ console.log(john.returnBalance);
 var myBalanceMethod = john.askTeller();
 var myBalance = myBalanceMethod();
 console.log(myBalance);
+--------------------------------
+function Person(first,last,age) {
+   this.firstname = first;
+   this.lastname = last;
+   this.age = age;
+   var bankBalance = 7500;
+
+   this.askTeller = function(pass) {
+     if (pass == 1234) return bankBalance;
+     else return "Wrong password.";
+   };
+}
+
+var john = new Person('John','Smith',30);
+/* the variable myBalance should access askTeller()
+   with a password as an argument  */
+var myBalance = john.askTeller(1234);
