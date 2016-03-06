47463ceb796c8d5a9e603c59b8caea1b761a498f
philiptang42 <philiptang42@gmail.com>
2016-03-05 23:01:13 -0500

    private variables, public method

--- OOP_js.js
+++ OOP_js.js
@@ -397,3 +397,22 @@ console.log(prototypeType);
 // now let's examine it!
 var hasOwn = Object.prototype.hasOwnProperty("hasOwnProperty")
 console.log(hasOwn);
+-------------------------------
+function StudentReport() {
+    var grade1 = 4;
+    var grade2 = 2;
+    var grade3 = 1;
+    this.getGPA = function() {
+        return (grade1 + grade2 + grade3) / 3;
+    };
+}
+
+var myStudentReport = new StudentReport();
+
+for(var x in myStudentReport) {
+    if(typeof myStudentReport[x] !== "function") {
+        console.log("Muahaha! " + myStudentReport[x]);
+    }
+}
+
+console.log("Your overall GPA is " + myStudentReport.getGPA());
