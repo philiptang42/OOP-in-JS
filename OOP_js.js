86abd5ab28c952d1b8fab596f7c8f5dafe5810c3
philiptang42 <philiptang42@gmail.com>
2016-03-05 22:54:00 -0500

    prototype of a base object

--- OOP_js.js
+++ OOP_js.js
@@ -389,3 +389,11 @@ yourDog.sayHello();
 
 var myDog = new Dog("dachshund");
 myDog.sayHello();
+-------------------------
+// what is this "Object.prototype" anyway...?
+var prototypeType = typeof Object.prototype
+console.log(prototypeType);
+
+// now let's examine it!
+var hasOwn = Object.prototype.hasOwnProperty("hasOwnProperty")
+console.log(hasOwn);
